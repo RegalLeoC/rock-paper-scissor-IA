59177bc2e2db0e8c666bc6c4c0b7148bb074a4bf
Victor Eduardo Garcia Sardaneta <regulusleoc@gmail.com>
2023-12-16 09:50:18 -0600

    Game rules (nonfunctional)

--- play.py
+++ play.py
@@ -2,6 +2,7 @@ from keras.models import load_model
 import cv2
 import numpy as np
 from random import choice
+import time
 
 REV_CLASS_MAP = {
     0: "rock",
@@ -12,10 +13,8 @@ REV_CLASS_MAP = {
     5: "thumbs_down"
 }
 
-
 def mapper(val):
     return REV_CLASS_MAP[val]
-
 
 def calculate_winner(move1, move2):
     if move1 == move2:
@@ -38,25 +37,45 @@ def calculate_winner(move1, move2):
             return "User"
         if move2 == "rock":
             return "Computer"
-        
+
     return "Unknown"
 
+def display_start_game(frame):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame, "Starting Game", (300, 300), font, 2, (0, 255, 0), 4, cv2.LINE_AA)
+    cv2.imshow("Rock Paper Scissors", frame)
+    cv2.waitKey(1000)
+
+def display_end_game(frame, score):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame, "Game Over", (300, 300), font, 2, (0, 0, 255), 4, cv2.LINE_AA)
+    cv2.putText(frame, f"Score: {score['wins']} wins, {score['losses']} losses, {score['ties']} ties",
+                (150, 400), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.imshow("Rock Paper Scissors", frame)
+    cv2.waitKey(3000)
 
 model = load_model("rock-paper-scissors-model.h5")
 
 cap = cv2.VideoCapture(0)
 
 prev_move = None
+score = {'wins': 0, 'losses': 0, 'ties': 0}
+total_games = 0
+max_games_per_session = 5  # Adjust as needed
+game_in_progress = False
 
 while True:
     ret, frame = cap.read()
     if not ret:
         continue
 
+    if not game_in_progress:
+        display_start_game(frame)
+        game_in_progress = True
+
     # rectangle for user to play
-    # cv2.rectangle(frame, (100, 100), (500, 500), (255, 255, 255), 2)
-    cv2.rectangle(frame, (30, 70), (230, 270), (255, 255, 255), 2)    
-    
+    cv2.rectangle(frame, (30, 70), (230, 270), (255, 255, 255), 2)
+
     # rectangle for computer to play
     cv2.rectangle(frame, (400, 70), (600, 270), (255, 255, 255), 2)
 
@@ -75,47 +94,35 @@ while True:
         if user_move_name != "none":
             computer_move_name = choice(['rock', 'paper', 'scissors'])
             winner = calculate_winner(user_move_name, computer_move_name)
-        else:
-            computer_move_name = "none"
-            winner = "Waiting..."
+
+            # Display computer's move for a short time
+            cv2.putText(frame, f"Computer's Move: {computer_move_name}",
+                        (500, 300), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.imshow("Rock Paper Scissors", frame)
+            cv2.waitKey(1500)  # Adjust the time to display the computer's move
+
+            if winner == "User":
+                score['wins'] += 1
+            elif winner == "Computer":
+                score['losses'] += 1
+            else:
+                score['ties'] += 1
+
+            total_games += 1
+            if total_games == max_games_per_session:
+                display_end_game(frame, score)
+                break
+
+            game_in_progress = False
+
     prev_move = user_move_name
 
     # display the information
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(frame, "Your Move: " + user_move_name,
                 (50, 50), font, 1.2, (255, 255, 255), 2, cv2.LINE_AA)
-    cv2.putText(frame, "Computer's Move: " + computer_move_name,
-                (750, 50), font, 1.2, (255, 255, 255), 2, cv2.LINE_AA)
     cv2.putText(frame, "Winner: " + winner,
                 (400, 600), font, 2, (0, 0, 255), 4, cv2.LINE_AA)
-
-    if computer_move_name != "none":
-        icon = cv2.imread("images/{}.png".format(computer_move_name))
-        if icon is not None:
-            icon = cv2.resize(icon, (400, 400))
-
-            # Get the dimensions of the icon
-            icon_height, icon_width, _ = icon.shape
-
-            # Get the region in the frame where the icon will be placed
-            region = frame[50:250, 500:700]
-
-            # Check if the region is not empty
-            if not region.size == 0:
-                # Resize the icon to match the dimensions of the region
-                resized_icon = cv2.resize(icon, (200, 200))
-                
-                # Calculate the position to place the resized icon
-                y_offset = 70
-                x_offset = 400
-               
-                frame[y_offset:y_offset + resized_icon.shape[0], x_offset:x_offset + resized_icon.shape[1]] = resized_icon
-
-            else:
-                print("Error: Region is empty.")
-        else:
-            print("Error loading image for {}".format(computer_move_name))
-
 
     cv2.imshow("Rock Paper Scissors", frame)
 
